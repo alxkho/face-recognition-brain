e85dde0e54b6d046b148b2a4dc436706e170b831
alxkho <alexa.khokhlova13@gmail.com>
2022-02-07 15:04:52 +0300

    added logo

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,13 +1,13 @@
 
-
 import "./Logo.css"
+import brain from "./brain.png"
 import Tilt from 'react-parallax-tilt';
 
 const Logo = () => {
     return (
         <Tilt className="br2 shadow-2 tilt">
-            <div className="" >
-                <h1>brain.png</h1>
+            <div className="Tilt-inner pa1" >
+                <img alt="logo" src={brain}></img>
             </div>
         </Tilt>
     )
